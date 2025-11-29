79bb487df2b3ec5d87c758dfe7a097b8867d2e05
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-11-29 22:08:41 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,1 @@
-// Cargo.toml
-// [dependencies]
-// eyre = "0.6"
-
-use eyre::{Result, eyre};
-
-fn main() {
-    let err: Result<()> = Err(eyre!("Something went wrong!"));
-
-    // Print the error
-    match err {
-        Ok(_) => println!("No error"),
-        Err(e) => println!("Error: {}", e),
-    }
-}
+fn main() {}
