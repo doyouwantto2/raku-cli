52828f71e85a06767816fc5c73878d08999a19b4
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-12-09 17:39:50 +0700

    remove salamander bz2 file from stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-//! # My crate
-//!
-//! `my crate is a collection of`
-//! calculation
-/// Add one doc test
-/// # example
-/// ```
-/// let arg = 5;
-/// let answer = raku::*;
-/// ```
-
 fn main() {}
