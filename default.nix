7c9056c79c88f2ba49917cba8318da2644c2597a
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2025-12-14 15:56:34 +0700

    merge with bevy, remove ratatui

--- default.nix
+++ default.nix
@@ -1,14 +1,47 @@
 { pkgs ? import <nixpkgs> { } }:
 
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.pkg-config
-    pkgs.openssl
-    pkgs.cmake
-    pkgs.alsa-oss
-    pkgs.alsa-lib
-    pkgs.alsa-utils
-    pkgs.rustc
-    pkgs.cargo
+let
+  runtimeLibs = with pkgs; [
+    libxkbcommon
+    wayland
+    wayland-protocols
+
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+
+    alsa-lib
+    udev
+
+    vulkan-loader
+
+    openssl
   ];
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rustc
+    cargo
+    cargo-edit
+    cargo-watch
+
+    clang
+    lld
+    pkg-config
+
+    vulkan-tools
+    vulkan-headers
+    vulkan-validation-layers
+  ] ++ runtimeLibs;
+
+  shellHook = ''
+    # Runtime dynamic libraries (winit / bevy)
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}"
+
+    # OpenSSL (common Rust crates expect these)
+    export OPENSSL_DIR=${pkgs.openssl.dev}
+    export OPENSSL_LIB_DIR=${pkgs.openssl.out}/lib
+    export OPENSSL_INCLUDE_DIR=${pkgs.openssl.dev}/include
+  '';
 }
